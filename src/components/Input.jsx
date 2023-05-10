1c17fbf523f6a32d80e5b0f58f7e15b47ff914dc
Olena Yastrebtsova <uaalena202@gmail.com>
2023-05-10 15:35:10 +0300

    new file

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,12 +2,15 @@
 
 
 
-const Input= () => {
+const Input = () => {
     return (
-        <div className='mt-10 flex flex-col'>
-            <input className='w-full h-12 mb-2 pl-4 rounded' type='text' placeholder='Email Address'>
+        <div className='mt-10 flex flex-col md:flex-row'>
+            <input className='w-full h-12 mb-2 pl-4 rounded md:rounded-l-lg md:w-[75%]' type='text' placeholder='Email Address'>
             </input>
-            <button>Join</button>
+            <div className='md:w-[24%]'>
+                <button className=' w-full bg-fuchsia-300 uppercase text-slate-600 font-inter font-bold h-12 rounded hover:bg-fuchsia-600 hover:text-slate-100 transform hover:duration-700 '>Join</button>
+            </div>
+
         </div>
     )
 }
--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -4,7 +4,7 @@ import Email from './Input'
 const Subscribe = () => {
   return (
     <section className='my-20 py-20 w-full  bg-[#80c8d5]'>
-        <div className=' container px-6'>
+        <div className=' container px-6 lg:w-[66%] lg:mx-auto'>
             <div className=' text-center '>
                 <h2 className='px-9 mb-4 font-bold text-4xl md:text-7xl'>Deliciousness to your inbox</h2>
                 <p className='font-inter mb-4'>Enjoy weekly hand picked recipes and recommendations</p>
