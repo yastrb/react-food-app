87bc9fbe36fc80bd27695d0f88e74c73b58c51fe
Olena Yastrebtsova <uaalena202@gmail.com>
2023-06-08 15:12:37 +0300

    new commit

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,17 +1,21 @@
-﻿import React from 'react';
-import { useEffect, useState } from 'react';
+﻿import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Recipe() {
-
     let params = useParams();
     const [details, setDetails] = useState({});
+    const [isActiveInstructions, setIsActiveInstructions] = useState(false);
+    const [isActiveIngredients, setIsActiveIngredients] = useState(false);
 
     const fetchDetails = async () => {
-        const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`);
+        const data = await fetch(
+            `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`
+        );
         const detailData = await data.json();
         setDetails(detailData);
-    }
+        console.log(params);
+        console.log(detailData);
+    };
 
     useEffect(() => {
         fetchDetails();
@@ -20,7 +24,9 @@ function Recipe() {
 
     const fetchCalories = async () => {
         try {
-            const response = await fetch(`https://api.spoonacular.com/recipes/${params.name}/nutritionWidget.json?apiKey=${process.env.REACT_APP_API_KEY}`);
+            const response = await fetch(
+                `https://api.spoonacular.com/recipes/${params.name}/nutritionWidget.json?apiKey=${process.env.REACT_APP_API_KEY}`
+            );
 
             if (!response.ok) {
                 throw new Error('Помилка отримання даних');
@@ -30,26 +36,56 @@ function Recipe() {
             const calories = data.calories;
             console.log('Калорії:', calories);
         } catch (error) {
-            console.error('Помилка під час отримання калорійності рецепту:', error);
+            console.error(
+                'Помилка під час отримання калорійності рецепту:',
+                error
+            );
         }
+    };
+
+    const handleInstructionsClick = () => {
+        setIsActiveInstructions(true);
+        setIsActiveIngredients(false); // Зміна стану другої кнопки на неактивний
+    };
+
+    const handleIngredientsClick = () => {
+        setIsActiveIngredients(true);
+        setIsActiveInstructions(false); // Зміна стану першої кнопки на неактивний
     };
 
     return (
         <div>
-            <section className=' container mx-auto px-4 mt-10 mb-5'>
-                <div className=' flex'>
-                    <div>
-                        <h2>{details.title}</h2>
-                        <img src={details.image} alt={details.title} />
+            <section className='container mx-auto px-4 mt-10 mb-5'>
+                <div className='flex flex-col gap-8 text-center '>
+                    <div className=' w-full'>
+                        <h2 className='mb-4 font-black text-2xl'>{details.title}</h2>
+                        <img className=' w-full ' src={details.image} alt={details.title} />
                     </div>
-                    <div>
-                        <button className=' py-1 px-2 text-[#313131] bg-white border border-black border-2'>Instructions</button>
-                        <button>Ingredients</button>
+                    <div className=''>
+                        <button
+                            className={`py-1 px-2 text-[#313131] border border-black border-2 mr-2 font-black ${isActiveInstructions ? 'bg-[#80c8d5]' : 'bg-white'
+                                }`}
+                            onClick={handleInstructionsClick}
+                            disabled={isActiveInstructions}
+                        >
+                            Instructions
+                        </button>
+                        <button
+                            className={`py-1 px-2 text-[#313131] border border-black border-2 mr-2 font-black ${isActiveIngredients ? 'bg-[#80c8d5]' : 'bg-white'
+                                }`}
+                            onClick={handleIngredientsClick}
+                            disabled={isActiveIngredients}
+                        >
+                            Ingredients
+                        </button>
+                        <div>
+                            {/* <h3 >{details.summary}</h3> */}
+                        </div>
                     </div>
                 </div>
             </section>
         </div>
-    )
+    );
 }
 
-export default Recipe
+export default Recipe;
